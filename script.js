5cb662074dfc60d86a2ad2771ee74561278f3edd
Bethfrantz <ehoyt@ivytech.edu>
2025-08-01 17:46:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,7 @@ document.getElementById("contactForm").addEventListener("submit", function(e)
 
 const name=document.getElementById("name").value;
 const email = document.getElementById("email").value;
-const subject = document.getElementById("subject").value;
 const message = document.getElementById("message").value;
-
-if (name === "" || email === "" || subject === "" || message === ""){
-  alert("All fields are required.");
-  return;
-}
-if (!isValidEmail(email)){
-  alert("Please enter a valid email address.");
-  return;
-}
-
-console.log("Form Data:");
-console.log ("Name:", name);
-console.log(Email:A", email);
-console.log("Subject:", subject);
-console.log("Message:", message);
 
 alert("Thank you for your message, ${name}!\n\nYour message: 
       "${message}"/n/nI will get back to you at ${email}!"}!);
