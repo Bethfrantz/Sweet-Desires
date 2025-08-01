398efcf10525328ab5156616efce009fd29f08f9
Bethfrantz <ehoyt@ivytech.edu>
2025-08-01 19:49:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Elisabeth Hoyt-Frantz, SDEV 153, Final Project, August 1, 2025
 
-document.getElementById("contactForm").addEventListener("submit", function(e) 
+document.getElementById("contactForm").addEventListener("submit", function(e) {
                                                         
    e.preventDefault();
 
