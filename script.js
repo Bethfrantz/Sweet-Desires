8cddfc546ccbf737b38bc6144e72bacb64b09300
Bethfrantz <ehoyt@ivytech.edu>
2025-08-01 17:49:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Elisabeth Hoyt-Frantz, SDEV 153, Final Project, August 1, 2025
 
 document.getElementById("contactForm").addEventListener("submit", function(e) 
-                                                        [
+                                                        
    e.preventDefault();
 
 const name=document.getElementById("name").value;
@@ -9,7 +9,7 @@ const email = document.getElementById("email").value;
 const message = document.getElementById("message").value;
 
 alert("Thank you for your message, ${name}!\n\nYour message: 
-      "${message}"/n/nI will get back to you at ${email}!"}!);
+      "${message}"/n/nI will get back to you at ${email}!");
    this.reset();
 });
 
